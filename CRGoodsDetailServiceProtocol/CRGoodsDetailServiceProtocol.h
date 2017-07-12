41e2c05eea3f6711964c776c537c1e369ab00b79
nrh <nrh@nrhdeMacBook-Air.local>
2017-07-12 16:26:50 +0800

    log

---
+++ CRGoodsDetailServiceProtocol/CRGoodsDetailServiceProtocol.h
@@ -0,0 +1,16 @@
+//
+//  CRGoodsDetailServiceProtocol.h
+//  CRGoodsDetailServiceProtocol
+//
+//  Created by nrh on 2017/7/12.
+//  Copyright © 2017年 zt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface CRGoodsDetailServiceProtocol : NSObject
+
+- (UIViewController *)goodsDetailViewControllerWithGoodsId:(NSString*)goodsId goodsName:(NSString *)goodsName;
+
+@end
---
+++ CRGoodsDetailServiceProtocol/CRGoodsDetailServiceProtocol.m
@@ -0,0 +1,13 @@
+//
+//  CRGoodsDetailServiceProtocol.m
+//  CRGoodsDetailServiceProtocol
+//
+//  Created by nrh on 2017/7/12.
+//  Copyright © 2017年 zt. All rights reserved.
+//
+
+#import "CRGoodsDetailServiceProtocol.h"
+
+@implementation CRGoodsDetailServiceProtocol
+
+@end
